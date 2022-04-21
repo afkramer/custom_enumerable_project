3979282539487e36de696d270287e1e608166bc5
Adriana Kramer <adri.kramer@gmail.com>
2022-04-21 19:40:07 +0200

    Add Enumerable#my_each_with_index

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,16 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    if block_given?
+      # Figure out how to pass the element and index to the block
+      i = 0
+      self.my_each do |element|
+        yield element, i
+        i += 1
+      end
+    end
+    self
+  end
 end
 
 # You will first have to define my_each
